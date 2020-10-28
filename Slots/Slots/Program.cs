af9feeb33711b74093625bf75a867116a16757b8
anwatson <anwatson1982@gmail.com>
2020-10-28 21:33:58 +0000

    Diag Function been added

--- Slots/Slots/Program.cs
+++ Slots/Slots/Program.cs
@@ -24,8 +24,9 @@ namespace SlotMachine3
             var ranNum = new Random();
             int stack = 20;
             string play = "";
-            
+
             ///Displays welcome message with rules of the game and displays opening ammount of coins
+            ///User to press any key to enter game or x to exit game
             welcome(stack);
             Console.WriteLine($"Press any Key play press [x to quit]");
             play = Console.ReadLine();
@@ -33,15 +34,19 @@ namespace SlotMachine3
                 return;
             else
 
-            Console.Clear();
+                Console.Clear();
+            ///Main for Loop of game if there are coins in the stack gives the user an option to enter 1,2 or 3 coins
+            ///goes through program works out if there was a win or not tells user if they have won or not and returns to 
+            ///the beggining of the loop 
+            ///if coins reach zero then exit the program 
             for (stack = 20; stack > 0;)
             {
                 coinDisplay(stack);
                 ///Enter stake and uses totalCoins function to remove coins from coin stack
                 Console.WriteLine($"Enter stake you would like to play (1,2 or 3 coins)");
-                string playGame = Console.ReadLine();      
+                string playGame = Console.ReadLine();
                 int inputStake = Int32.Parse(playGame);
-                Console.Clear();            
+                Console.Clear();
                 ///Populates grid with random numbers from 0 - 2 
                 for (row = 0; row < 3; row++)
                 {
@@ -50,17 +55,18 @@ namespace SlotMachine3
                         grid[row, col] = ranNum.Next(0, 3);
                         Console.Write($" {grid[row, col]} ");
                         if (col == 2)
-                        Console.WriteLine();
-                        
+                            Console.WriteLine();
                     }
                 }
-
-            ///Works out winning lines from grid 
+                ///Works out winning lines from grid 
                 bool topLine = checkHorizontal(grid, 0);
                 bool midLine = checkHorizontal(grid, 1);
                 bool botLine = checkHorizontal(grid, 2);
-              ///Tells program what to do when each stake (1,2 or 3) is placed)
-                    if (inputStake == 1)
+                bool diagLefttoRight = DiagCheck(grid, checkDiag.leftToRight);
+                bool diagRighttoLeft = DiagCheck(grid, checkDiag.RightToLeft);
+
+                ///Tells program what to do when each stake (1,2 or 3) is placed)
+                if (inputStake == 1)
                 {
                     stack = totalCoins(stack, checkStake.oneCoin);
                     if (midLine == true)
@@ -81,18 +87,16 @@ namespace SlotMachine3
                 if (inputStake == 3)
                 {
                     stack = totalCoins(stack, checkStake.threeCoins);
-                    if (topLine || botLine || midLine == true)
+                    if (topLine || botLine || midLine || diagLefttoRight || diagRighttoLeft == true)
                     {
-                        stack = calcWinnings(inputStake, stack, checkStake.threeCoins); 
+                        stack = calcWinnings(inputStake, stack, checkStake.threeCoins);
                     }
-                    
-
                 }
                 //Displays win or lose depending on if there were any winning lines or not
-                displayResult(topLine, botLine, midLine);
-
+                displayResult(topLine, botLine, midLine, diagLefttoRight, diagRighttoLeft);
             }
         }
+  
 
         /// <summary>
         /// Function to dispay ammount of coins users has 
@@ -145,13 +149,14 @@ namespace SlotMachine3
             else
                 return 0;
         }
+    
         /// <summary>
         /// Checks to see if stake has won and returns True if user has won or false if user has not won
         /// </summary>
         /// <param name="gameGrid">Grid of numbers (grid)</param>
         /// <param name="winRow">Which row has all the same numbers</param>
         /// <returns>True or False</returns>
-        static bool checkHorizontal(int[,] gameGrid, int winRow)
+        static bool checkHorizontal(int[,] gameGrid, int winRow )
         {
             if (gameGrid[winRow, 0] == gameGrid[winRow, 1] && gameGrid[winRow, 1] == gameGrid[winRow, 2])
             {
@@ -164,14 +169,47 @@ namespace SlotMachine3
             
         }
         /// <summary>
-        /// Displays win or lose message 
+        /// Check if Diagnal lines are winning lines, using righttoleft and lefttoright enum 
         /// </summary>
-        /// <param name="top">top (topLine) line of the grid</param>
-        /// <param name="middle">Middle line (midLine) of the grid</param>
-        /// <param name="bottom">Bottom line of the grid (botLine)</param>
-        static void displayResult (bool top, bool middle, bool bottom)
+        /// <param name="gameGrid">The Game Grid</param>
+        /// <param name="DiagLine">Check left to Right and Right to Left diag lines</param>
+        /// <returns>True or False if Lines are winning lines or not </returns>
+        static bool DiagCheck(int[,] gameGrid, checkDiag DiagLine)
         {
-            if (top || middle || bottom == true)
+            if (DiagLine == checkDiag.leftToRight)
+            {
+                if (gameGrid[0, 0] == gameGrid[1, 1] && gameGrid[1, 1] == gameGrid[2, 2])
+                    return true;
+                else
+                {
+                    return false;
+                }
+            }
+                if (DiagLine == checkDiag.RightToLeft)
+                {
+                    if (gameGrid[0, 2] == gameGrid[1, 1] && gameGrid[1, 1] == gameGrid[2, 0])
+
+                        return true;
+                    else
+                    {
+                        return false;
+                    }
+
+                }
+                else
+                {
+                    return false;
+                }
+            }
+            /// <summary>
+            /// Displays win or lose message 
+            /// </summary>
+            /// <param name="top">top (topLine) line of the grid</param>
+            /// <param name="middle">Middle line (midLine) of the grid</param>
+            /// <param name="bottom">Bottom line of the grid (botLine)</param>
+            static void displayResult (bool top, bool middle, bool bottom, bool DiagLtoR, bool DiagRtoL)
+        {
+            if (top || middle || bottom || DiagLtoR || DiagRtoL == true)
             {
                 Console.WriteLine($"You Won =)");
             }
@@ -208,6 +246,6 @@ namespace SlotMachine3
             }
         }
 
-    }///TODO: Creat function for Diag lines 
-     ///TODO: Clean up exit of game display total winning coins when program exitedby player 
 
+    }
+     ///TODO: Clean up exit of game display total winning coins when program exitedby player 
